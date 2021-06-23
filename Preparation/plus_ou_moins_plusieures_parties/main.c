d3041d9a802ddb0a7d13c457e2e6209cd14436ba
jerembrbzt <barbezat.jeremy@gmail.com>
2021-06-23 19:30:40 +0200

    deux joueurs

--- Preparation/plus_ou_moins_plusieures_parties/main.c
+++ Preparation/plus_ou_moins_plusieures_parties/main.c
@@ -1,38 +1,49 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-void jouer_une_partie()
+void jouer_une_partie(int nombreMystere)
 {
-    const int MAX = 1, MIN = 1;
-    int nombreMystere = 0, nombreEntre = 0, compteCoups = 0;
-    nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+    int proposition = 0, compteCoups = 0;
     do
     {
-        printf("Quel est le nombre ? ");
-        scanf("%d", &nombreEntre);
+        printf("Quelle est votre proposition ? ");
+        scanf("%d", &proposition);
         compteCoups++;
 
-        if (nombreMystere > nombreEntre)
+        if (nombreMystere > proposition)
             printf("C'est plus !\n\n");
-        else if (nombreMystere < nombreEntre)
+        else if (nombreMystere < proposition)
             printf("C'est moins !\n\n");
-        else
+        else {
             printf("Bravo, le nombre mystere etait bien %d, vous l'avez trouve en %d coups !",
                    nombreMystere, compteCoups);
-    } while (nombreEntre != nombreMystere);
+            return;
+        }
+    } while (1);
+}
+
+int genere_nombre_mystere()
+{
+    const int MIN = 1, MAX = 5;
+    srand(time(NULL));
+    return (rand() % (MAX - MIN + 1)) + MIN;
+}
+
+int demande_nombre_mystere()
+{
+    int nombreMystere;
+    printf("Entrez le nombre mystere (l'autre joueur ne doit pas regarder)");
+    scanf("%d", &nombreMystere);
+    return nombreMystere;
 }
 
 int main(int argc, char **argv)
 {
     int continuer;
-
-    srand(time(NULL));
-
     do
     {
-        jouer_une_partie();
+        jouer_une_partie(demande_nombre_mystere());
         printf("veux tu faire une nouvelle partie ?\n0. Non\n1. Oui\n");
         scanf("%d", &continuer);
     } while (continuer);
