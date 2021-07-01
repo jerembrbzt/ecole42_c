03d95fd1d1e1b1619fc7cecf86df63e350b5f553
jerembrbzt <barbezat.jeremy@gmail.com>
2021-07-01 11:11:52 +0300

    les_pointeurs_jeremy

--- Preparation/Les_pointeurs/main.c
+++ Preparation/Les_pointeurs/main.c
@@ -7,10 +7,10 @@ int main(int argc, char *argv[])
 {
     int heures = 0, minutes = 90;
 
-    // On envoie l'adresse de heures et minutes
+    // envoie l'adresse de heures et minutes
     decoupeMinutes(&heures, &minutes);
 
-    // Cette fois, les valeurs ont été modifiées !
+    // les valeurs ont été modifiées !
     printf("%d heures et %d minutes", heures, minutes);
 
     return 0;
@@ -18,10 +18,7 @@ int main(int argc, char *argv[])
 
 void decoupeMinutes(int* pointeurHeures, int* pointeurMinutes)
 {
-    /* Attention à ne pas oublier de mettre une étoile devant le nom
-    des pointeurs ! Comme ça, vous pouvez modifier la valeur des variables,
-    et non leur adresse ! Vous ne voudriez pas diviser des adresses,
-    n'est-ce pas ? ;o) */
+    
     *pointeurHeures = *pointeurMinutes / 60;
     *pointeurMinutes = *pointeurMinutes % 60; 
 }
